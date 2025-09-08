669d67f409a67483955e20e45f26d5924181d560
Yogesh.M <yoge.inespanol@gmail.com>
2025-09-08 11:42:45 +0530

    Update led_pingpong.c
    This program creates a back-and-forth LED chaser using 8 LEDs connected to P0.0 to P0.7 on the LPC2129 microcontroller.
    All 8 pins are set as outputs using IODIR0 = 0xFF.
    LEDs light up one at a time from left (P0.0) to right (P0.7), then reverse back from right to left.
    delay_ms() uses Timer 0 to create accurate millisecond delays between LED transitions.
    IOSET0 turns ON a specific LED; IOCLR0 turns it OFF after the delay.
    This pattern runs infinitely inside the while(1) loop.

--- LED/led_pingpong.c
+++ LED/led_pingpong.c
@@ -1,23 +1,35 @@
 #include<LPC21xx.h>
-#include"delay.h"
+void delay_ms(unsigned int);
+
+void delay_ms(unsigned int ms){
+T0PR=15000-1;
+T0TCR=0x01;
+while(T0TC<ms);
+T0TCR=0x03;
+T0TCR=0x00;
+}
+
 int main(){
  int i,j;
 
 while(1)
 {
 IODIR0=0xff;
+ 
 for(i=0;i<8;i++)
 {
 IOSET0=1<<i;
 delay_ms(60);
 IOCLR0=1<<i;
 }
+ 
 for(j=6;j>=0;j--)
 {
 IOSET0=1<<j;
 delay_ms(60);
 IOCLR0=1<<j;
+      }
+   }
 }
-}
-}
+
 
